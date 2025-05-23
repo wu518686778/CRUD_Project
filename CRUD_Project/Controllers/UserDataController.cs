4881ba1124c9c18e386da7aca4259fb89d50f051
wu518686778 <wu518686778@hotmail.com>
2025-05-23 16:25:28 +0800

    完善CRUD功能

--- CRUD_Project/Controllers/UserDataController.cs
+++ CRUD_Project/Controllers/UserDataController.cs
@@ -9,9 +9,8 @@ namespace CRUD_Project.Controllers
     public class UserDataController : Controller
     {
 
-        //*****連結MVC_UserDB資料庫*****[Start]
+        //連結MVC_UserDB資料庫
         private readonly MvcUserDbContext _db;
-        //*****連結MVC_UserDB資料庫*****[End]
 
         private readonly ILogger<UserDataController> _logger;
         public UserDataController(ILogger<UserDataController> logger, MvcUserDbContext DbContext)
@@ -20,8 +19,127 @@ namespace CRUD_Project.Controllers
             _db = DbContext;
         }
  
-        public async Task<IActionResult> UserList()
+        public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost,ActionName("Create")]
+        [ValidateAntiForgeryToken] // 避免CSRF攻擊
+        public IActionResult Create([Bind("UserId, UserName, UserSex, UserBirthDay, UserMobilePhone")] UserTable _userTable)
+        {
+
+            if (_userTable != null && ModelState.IsValid) {  //ModelState.IsValid 通過表單驗證
+
+                _db.UserTables.Add(_userTable);  //將資料加入到資料庫的UserTable中
+                _db.SaveChanges();               //儲存變更到資料庫中
+                return RedirectToAction(nameof(List));  //導向List()方法
+            }
+            else
+            {
+                ModelState.AddModelError("", "請檢查輸入的資料是否正確！");   // 第一個輸入值是 key，第二個是錯誤訊息（字串）
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? _ID)
+        {
+            if (_ID == null)
+            {   
+                return new StatusCodeResult((int)System.Net.HttpStatusCode.BadRequest);
+            }
+
+            UserTable? ut = _db.UserTables.Find(_ID);
+
+            if (ut == null)
+            {   
+                return NotFound();
+            }
+            else
+            {
+                return View(ut);
+            }
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditConfirm([Bind("UserId, UserName, UserSex, UserBirthDay, UserMobilePhone")] UserTable _userTable)
+        {
+            if (ModelState.IsValid) //通過表單驗證
+            {
+                UserTable? ut = _db.UserTables.Find(_userTable.UserId);
+                if (ut == null)
+                {   
+                    return NotFound();
+                }
+                else
+                {
+                    ut.UserName = _userTable.UserName; 
+                    ut.UserSex = _userTable.UserSex;
+                    ut.UserBirthDay = _userTable.UserBirthDay;
+                    ut.UserMobilePhone = _userTable.UserMobilePhone;
+                    _db.SaveChanges(); 
+                }
+                return RedirectToAction(nameof(List)); 
+            }
+            else
+            {
+                return View(_userTable);
+            }
+        }   
+
+        public IActionResult Delete(int? _ID)
+        {
+            if (_ID == null || _ID.HasValue == false)
+            {
+                return Content("沒有輸入編號（_ID）");
+            }
+
+            UserTable? ut = _db.UserTables.Find(_ID);
+
+            if (ut == null)
+            {   
+                return NotFound();
+            }
+            else
+            {
+                return View(ut);
+            }
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int? _ID)
+        {
+            if (ModelState.IsValid)
+            {
+                UserTable? ut = _db.UserTables.Find(_ID);
+                if (ut == null)
+                {  
+                    return Content(" 刪除時，找不到這一筆記錄！");
+                }
+                else
+                {
+                    _db.Entry(ut).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;  
+                    _db.SaveChanges();
+                }
+
+                return RedirectToAction(nameof(List));
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "編號（_ID）驗證失敗！");
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> List()
+        {
+            //https://hackmd.io/@AndyShih/SJOlljYI2
+            //在IDE中寫好的IQueryable只是"查詢狀態"，此時還沒執行資料庫的查詢，因此不會有資料載入記憶體的行為。
+            //若指派某些會得到"明確結果"的function，如Count()、ToList()等，此時才會執行SQL查詢指令，取得查詢結果。
             IQueryable<UserTable> ListAll = from m in _db.UserTables
                                                                     select m;
 
@@ -38,12 +156,12 @@ namespace CRUD_Project.Controllers
         }
 
         [HttpGet]
-        public IActionResult Details(int? _ID=1)
+        public IActionResult Details(int? _ID)
+        {
+            if (_ID == null || _ID.HasValue == false)
             {
-            //if (_ID == null || _ID.HasValue == false)
-            //{
-            //    return Content("沒有輸入編號（_ID）");
-            //}
+                return Content("沒有輸入編號（_ID）");
+            }
 
             IQueryable<UserTable> ListOne = from m in _db.UserTables
                                                                        where m.UserId == _ID    
--- CRUD_Project/Program.cs
+++ CRUD_Project/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{_ID?}");
 
 app.Run();
