4d000b02de6df8d210c2f9a23f44fedd66ceec7e
wu518686778 <wu518686778@hotmail.com>
2025-06-02 18:00:18 +0800

    新增Sidebar樣式至_Layout2

---
+++ CRUD_Project/Views/Shared/_Layout2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Project.Views.Shared
+{
+    public class _Layout2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
